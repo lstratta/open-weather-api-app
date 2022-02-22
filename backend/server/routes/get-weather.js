74587fbf31ce08d823379b20da5b666b2f775cf2
Luke Stratta <mrlukestratta@gmail.com>
2022-02-22 23:33:49 +0000

    updated scripts

---
+++ backend/server/routes/get-weather.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config( { path: `ENV_VARIABLES/.env.${process.env.NODE_ENV}` })
+
+const router = express.Router();
+
+router.route('/').get(async(req, res) => {
+    res.status(200).json("Page found");
+})
+
+router.route('/current-weather/:your-location').get( async (req, res) => {
+
+
+})
+
+router.route('/current-weather/:city/:units').get( async (req, res) => {
+
+    // sanitise inputs
+
+    // make request
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.NODE_ENV}&units=${req.params.units}`)
+        .then(data => {
+            res.send(data.data);
+        })
+        .catch(err => {
+            res.status(404).send({error: "That location hasn't been found"});
+        });
+})       
+
+router.route('/current-weather/:city/:country/:units/').get( async (req, res) => {
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city},${req.params.country}&appid=${process.env.NODE_ENV}&units=${req.params.units}`)
+        .then(data => {
+            res.send(data.data);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+})       
+
+
+
+module.exports = router;
---
+++ backend/test/get-weather.test.js
@@ -0,0 +1,75 @@
+const { expect, request, assert } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = require('chai').should();
+
+chai.use(chaiHttp);
+
+const server = require('../server/server');
+
+describe('Test Suite', () => {
+    
+    describe('Testing getting data from OpenWeather API', () => {
+
+        it('TEST 1: Can make a GET request to backend server', async () => {
+
+            const res = await chai.request(server)
+                            .get('/')
+                            .send();
+            
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.a('string').eql('Page found')
+        })
+
+        it('TEST 2: Can make GET request to OpenWeather API for current day data at specific location', async () => {
+
+            res = await chai.request(server)
+                    .get('/current-weather/london/uk/metric')
+                    .send();
+
+            expect(res).to.have.status(200);
+            (res.body).should.have.property('coord');
+        })
+
+        
+
+        // TEST 4 Inputs are sanit
+ 
+    })
+
+    describe(`Testing error handling`, () => {
+
+        it ('TEST 1: An incorrect city name returns a 404 status code', async() => {
+
+            res = await chai.request(server)
+                .get('/current-weather/lnodon/metric')
+                .then( res => {
+                    expect(res).to.have.status(404);
+                    expect(res.body).to.have.property("error").eql("That location hasn't been found");
+                 })
+                 .catch( err => {
+                    throw err;
+                 });
+        })
+
+    })
+
+    describe(`Testing user inputs are sanitised`, async => {
+
+        xit('TEST 1: User inputs are only allowed to be alphabetical characters', () => {
+
+        })
+    })
+
+    describe(`Testing database integration`, () => {
+
+        xit(`TEST 1: GET request should return sampleData`, async () => {
+
+            const res = await chai.request(server)
+                .get(`/`)
+                .send();
+
+            expect(res).to.have.status(200);
+        })
+    })
+})
