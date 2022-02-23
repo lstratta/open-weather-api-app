c8d628ca32c50839448253420358cd52baee7e49
Luke Stratta <mrlukestratta@gmail.com>
2022-02-23 13:19:22 +0000

    updated scripts

--- ci-cd/scripts/deploy.sh
+++ ci-cd/scripts/deploy.sh
@@ -4,18 +4,21 @@ bash <<EOF
 
     #!/bin/sh
 
+    cd /home/ubuntu/app/open-weather-api-app
+
     pwd
 
-    git pull origin main
-
-    nvm install --lts
-
-    npm install --production
-
-    npm install pm2 -g &
-
-    pm2 kill
-
-    pm2 start backend/server/server.js --watch
-
 EOF
+
+
+# git pull origin main
+
+    # nvm install --lts
+
+    # npm install --production
+
+    # npm install pm2 -g &
+
+    # pm2 kill
+
+    # pm2 start backend/server/server.js --watch
