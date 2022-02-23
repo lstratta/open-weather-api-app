4df48e4b7439d675d1c3dd4a2f67a7205b78f816
Luke Stratta <mrlukestratta@gmail.com>
2022-02-23 11:23:33 +0000

    updated scripts

--- ci-cd/scripts/deploy.sh
+++ ci-cd/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bash <<EOF 
+sudo bash <<EOF 
 
     #!/bin/sh
 
@@ -8,17 +8,16 @@ bash <<EOF
 
     pwd
 
-    sudo -S git pull origin main
+    git pull origin main
 
-    sudo -S nvm install --lts
+    nvm install --lts
 
-    sudo -S npm install --production
+    npm install --production
 
-    sudo -S npm install pm2 -g
+    npm install pm2 -g
 
     pm2 kill
 
     pm2 start backend/server/server.js --watch
 
 EOF
-/etc/sudoers.d/
