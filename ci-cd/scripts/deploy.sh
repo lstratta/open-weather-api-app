5f0f86f0ad4d22b9c5611526b7105c828a3b7d33
Luke Stratta <mrlukestratta@gmail.com>
2022-02-23 11:21:22 +0000

    updated scripts

--- ci-cd/scripts/deploy.sh
+++ ci-cd/scripts/deploy.sh
@@ -8,13 +8,13 @@ bash <<EOF
 
     pwd
 
-    sudo git pull origin main
+    sudo -S git pull origin main
 
-    sudo nvm install --lts
+    sudo -S nvm install --lts
 
-    sudo npm install --production
+    sudo -S npm install --production
 
-    sudo npm install pm2 -g
+    sudo -S npm install pm2 -g
 
     pm2 kill
 
