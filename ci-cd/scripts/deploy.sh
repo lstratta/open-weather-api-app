9997d7d63c08155a492b8dcfa949c3cb04f4d436
Luke Stratta <mrlukestratta@gmail.com>
2022-02-23 00:40:19 +0000

    updated scripts

--- ci-cd/scripts/deploy.sh
+++ ci-cd/scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-cd /var/lib/jenkins/workspace/OpenWeatherApp
+cd ~/app
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
-. ~/.nvm/nvm.sh
+git pull origin main
 
 sudo nvm install --lts
 
-npm install
+npm install --production
 
 npm install pm2 -g
 
