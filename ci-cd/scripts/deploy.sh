657aa363e33910b5443c64b3adf8add94543dc96
Luke Stratta <mrlukestratta@gmail.com>
2022-02-23 10:22:35 +0000

    updated scripts

--- ci-cd/scripts/deploy.sh
+++ ci-cd/scripts/deploy.sh
@@ -6,10 +6,10 @@ git pull origin main
 
 nvm install --lts
 
-npm install --production
+sudo npm install --production
 
-npm install pm2 -g
+sudo npm install pm2 -g
 
-pm2 kill
+sudo pm2 kill
 
-pm2 start backend/server/server.js --watch
+sudo pm2 start backend/server/server.js --watch
